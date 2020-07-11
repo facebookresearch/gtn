4062b2af4d43189416fddd16667ee10331dd8c5d
Awni Hannun <awni@fb.com>
2020-07-11 14:14:02 -0400

    bindings + tests for graph scalar ops (#38)

--- bindings/python/gtn/_functions.cpp
+++ bindings/python/gtn/_functions.cpp
@@ -9,6 +9,7 @@ namespace py = pybind11;
 using namespace py::literals;
 
 PYBIND11_MODULE(_functions, m) {
+  m.def("add", add, "lhs"_a, "rhs"_a);
   m.def(
       "concat",
       py::overload_cast<const Graph&, const Graph&>(&concat),
@@ -21,6 +22,7 @@ PYBIND11_MODULE(_functions, m) {
   m.def("compose", compose, "first"_a, "second"_a);
   m.def("closure", closure, "graph"_a);
   m.def("forward_score", forwardScore, "graph"_a);
+  m.def("negate", negate, "input"_a);
   m.def("project_input", projectInput, "other"_a);
   m.def("project_output", projectOutput, "other"_a);
   m.def(
--- bindings/python/test/test_bindings.py
+++ bindings/python/test/test_bindings.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-import unittest
 from test_utils import GTNModuleTestCase
 
 try:
-    from gtn import *
+    import gtn
 except ImportError:
     print("Could not import gtn package - will skip tests")
 
 
 class GraphTestCase(GTNModuleTestCase):
     def setUp(self):
-        g = Graph(False)
+        g = gtn.Graph(False)
         g.add_node(True)
         g.add_node()
         g.add_node()
@@ -23,7 +22,6 @@ class GraphTestCase(GTNModuleTestCase):
         g.add_arc(1, 2, 0)
         g.add_arc(1, 1, 1, 1, 2.1)
         g.add_arc(2, 3, 2)
-        
         self.g = g
 
     def test_graph_basic(self):
@@ -39,7 +37,7 @@ class GraphTestCase(GTNModuleTestCase):
         self.assertEqual(len(weights), len(expected))
         for i in range(0, len(weights)):
             self.assertAlmostEqual(weights[i], expected[i], places=4)
-    
+
     def test_graph_weights_set(self):
         weights_original = self.g.weights()
         weights_new_expected = [1.1, -3.4, 0, 0.5, 0]
@@ -49,3 +47,40 @@ class GraphTestCase(GTNModuleTestCase):
         for i in range(0, len(weights_new)):
             self.assertAlmostEqual(weights_new[i], weights_new_expected[i], places=4)
         self.g.set_weights(weights_original)
+
+
+class FunctionsTestCase(GTNModuleTestCase):
+
+    def test_scalar_ops(self):
+        g1 = gtn.Graph()
+        g1.add_node(True)
+        g1.add_node(False, True)
+        g1.add_arc(0, 1, 0, 0, 1.0)
+
+        # Test negate:
+        res = gtn.negate(g1)
+        self.assertEqual(res.item(), -1.0)
+        gtn.backward(res)
+        self.assertEqual(g1.grad().item(), -1.0)
+        g1.zero_grad()
+
+        g2 = gtn.Graph()
+        g2.add_node(True)
+        g2.add_node(False, True)
+        g2.add_arc(0, 1, 0, 0, 3.0)
+
+        # Test add:
+        res = gtn.add(g1, g2)
+        self.assertEqual(res.item(), 4.0)
+        gtn.backward(res)
+        self.assertEqual(g1.grad().item(), 1.0)
+        self.assertEqual(g2.grad().item(), 1.0)
+        g1.zero_grad()
+        g2.zero_grad()
+
+        # Test subtract:
+        res = gtn.subtract(g1, g2)
+        self.assertEqual(res.item(), -2.0)
+        gtn.backward(res)
+        self.assertEqual(g1.grad().item(), 1.0)
+        self.assertEqual(g2.grad().item(), -1.0)
